deb547e8ca46557c61380b43e697c18c614c0b0d
RS Computers <blertoni2021@gmail.com>
2024-05-16 22:36:00 +0200

    Save Changes

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import Navbar from "./Components/Navbar/Navbar";
+import {Route} from "react-router-dom";
+
+function App() {
+  return (
+   <>
+     <Navbar/>
+
+   </>
+  );
+}
+
+export default App;
