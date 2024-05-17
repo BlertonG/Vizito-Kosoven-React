31f8e5c44203254252e4ed73689be9494074304b
RS Computers <blertoni2021@gmail.com>
2024-05-17 13:28:11 +0200

    Save Changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import './App.css';
 import Navbar from "./Components/Navbar/Navbar";
+
+
 import {Route} from "react-router-dom";
+import ImagesSlider from "./Components/Navbar/Home";
+import SliderComponent from "./Components/Navbar/Home";
+import CarouselPage from "./Components/Navbar/Home";
+import HomePage from "./Components/Navbar/Home";
+
+
 
 function App() {
   return (
    <>
      <Navbar/>
+       <HomePage/>
+
 
    </>
   );
